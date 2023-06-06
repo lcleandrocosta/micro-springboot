68ba1d2d8a72e8c0825e35916c9882692ff6c21f
lcleandrocosta <lcleandrocosta@gmail.com>
2023-06-06 13:09:17 -0300

    Project hr-worker created

---
+++ hr-woker/src/main/java/com/training/hrwoker/HrWokerApplication.java
@@ -0,0 +1,13 @@
+package com.training.hrwoker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrWokerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrWokerApplication.class, args);
+	}
+
+}
---
+++ hr-woker/src/test/java/com/training/hrwoker/HrWokerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.training.hrwoker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrWokerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
